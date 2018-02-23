3f642a9c01cdd3fe67a304fddd6f08d452c89126
Trung <trunglive@gmail.com>
2018-02-22 19:27:02 -0500

    fix(style): fix broken photo gallery

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 class HomeCard extends Component {
   render() {
     return (
-      <div className="featured-properties-card__home">
-        <img src='/images/home-1.jpg' alt='photo' />
+      <div className="featured-properties-card-container">
+        <img src="/images/home-1.jpg" alt="photo" />
+        <div className="featured-properties-card-container__info">
+          yesssssssss!
+        </div>
       </div>
-    )
+    );
   }
 }
 
