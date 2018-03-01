4a24f2c51c235905e52c855f11d3509da2bd6069
Trung <trunglive@gmail.com>
2018-03-01 16:48:35 -0500

    doc(footer): add author to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,10 +11,26 @@ class Footer extends Component {
             <div className="footer__nav--help">Help</div>
             <div className="footer__nav--social">
               <i className="footer__nav--social-facebook ion-social-facebook" />
-
               <i className="footer__nav--social-instagram ion-social-instagram-outline" />
             </div>
           </div>
+        </div>
+        <div className="author">
+          <p>
+            Code by{' '}
+            <a href="https://github.com/trunglive" target="_blank">
+              Trung Vo
+            </a>
+          </p>
+          <p>
+            Designed by{' '}
+            <a
+              href="https://dribbble.com/shots/4177962-Find-fancy-home-abroad/attachments/954897"
+              target="_blank"
+            >
+              Olga Radzikowna
+            </a>
+          </p>
         </div>
       </footer>
     );
