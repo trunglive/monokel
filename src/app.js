7530a686a826dedafa3564c6f5e7599b74d75270
Trung <trunglive@gmail.com>
2018-03-04 11:41:18 -0500

    refactor(style): fix some styling issues

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Homepage from './components/Homepage';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
