11156df3fdc5b1cd6ca9c744bc17d79c67e5b203
trunglive <trunglive@gmail.com>
2018-04-19 19:24:04 -0400

    feat(icons): add icons for horizontal scrolling

--- src/components/cheap-and-luxury/CheapLuxury.js
+++ src/components/cheap-and-luxury/CheapLuxury.js
@@ -4,7 +4,14 @@ import { cheapLuxury } from "../../utils/data";
 
 const CheapLuxury = () => (
   <div className="card cheap-luxury">
+    
+    <div className="title-and-arrow">
     <p className="card__title cheap-luxury__title">Cheap and Luxury</p>
+    <div className="arrow-group">
+      <img className="circle-arrow__icon arrow-left" src="/icons/arrow-right.svg" />
+      <img className="circle-arrow__icon arrow-right" src="/icons/arrow-right.svg" />
+    </div>
+  </div>
     <div className="card-list cheap-luxury-list">
       {cheapLuxury.map(home => <CheapLuxuryCard key={home.name} {...home} />)}
     </div>
--- src/components/newly-featured/NewlyFeatured.js
+++ src/components/newly-featured/NewlyFeatured.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import NewlyFeaturedCard from "./NewlyFeaturedCard";
 import { newlyFeatured } from "../../utils/data";
 
 const NewlyFeatured = () => (
-  <div className="card newly-featured">
-    <p className="card__title newly-featured__title">Newly Featured</p>
+  <div className="card newly-featured" id="card-list">
+    <div className="title-and-arrow">
+      <div className="card__title newly-featured__title">Newly Featured</div>
+      <div className="arrow-group">
+        <img
+          className="circle-arrow__icon arrow-left"
+          src="/icons/arrow-right.svg"
+        />
+        <img
+          className="circle-arrow__icon arrow-right"
+          src="/icons/arrow-right.svg"
+        />
+      </div>
+    </div>
+
     <div className="card-list newly-featured-list">
       {newlyFeatured.map(home => (
         <NewlyFeaturedCard key={home.name} {...home} />
